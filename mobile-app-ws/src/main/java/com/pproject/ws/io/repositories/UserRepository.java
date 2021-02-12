bf8371dc72df2738db30a96b187e6884686c254a
Admin <Admin@Admin-PC>
2021-02-13 00:50:36 +0530

    #second commit-Adding authorization filter

---
+++ mobile-app-ws/src/main/java/com/pproject/ws/io/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.pproject.ws;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.pproject.ws.io.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity,Long> {
+	UserEntity findByEmail(String email);
+
+}
